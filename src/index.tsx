b1565c0c7603c1a4fc148f5b3649aca1cba00054
Rogelio John Oliverio <110364637+rogeliojohnoliverio@users.noreply.github.com>
2023-05-08 12:00:05 +0800

    Setting up the router (#3)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './assets/css/index.css';
+import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
