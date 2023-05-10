c5e5f028284d11c08e2b14594358c3f78ce5d5a3
Brucenigel <brucenige.vilo@sun-asterisk.com>
2023-05-10 10:20:26 +0800

    replace svg

--- src/pages/UserInformation.tsx
+++ src/pages/UserInformation.tsx
@@ -2,11 +2,19 @@ import React, { FC } from 'react';
 import '../assets/css/UserInformation.css';
 import Layout from './components/Layout';
 import SunAsterisk from './SunAsterisk';
+import {
+  ArchiveBoxArrowDownIcon,
+  ArrowDownTrayIcon,
+  CalendarDaysIcon,
+  EnvelopeIcon,
+  HomeModernIcon,
+  PhoneIcon,
+} from '@heroicons/react/24/solid';
 
 const UserInformation: FC = () => {
   return (
     <Layout>
-      <main className='flex items-center flex-col justify-center bg-gray-200 p-28'>
+      <main className='flex items-center flex-col justify-center'>
         <SunAsterisk />
         <p className='text-gray-600 font-semibold text-4xl pt-5'>User Info</p>
         <div className='flex flex-row align border-1 shadow-md '>
@@ -20,22 +28,22 @@ const UserInformation: FC = () => {
               John <span className='suns-color'>Doe</span>
             </div>
             <div className='flex flex-row p-1.5'>
-              <img src={process.env.PUBLIC_URL + 'images/Home.svg'} alt='' />{' '}
+              <HomeModernIcon className='w-6 h6' />{' '}
               <span className='pl-6'> Nasipit, Talamban, Cebu</span>
             </div>
             <div className='flex flex-row p-1.5'>
-              <img src={process.env.PUBLIC_URL + 'images/Date.svg'} alt='' />{' '}
+              <CalendarDaysIcon className='w-6 h-6' />{' '}
               <span className='pl-6'> May, 5 2023</span>
             </div>
             <div className='text-xs font-semibold p-1.5'>
               <span className='suns-color'>CONTACT</span> INFORMATION
             </div>
             <div className='flex flex-row p-1.5'>
-              <img src={process.env.PUBLIC_URL + 'images/Email.svg'} alt='' />{' '}
+              <EnvelopeIcon className='w-6 h-6' />{' '}
               <span className='pl-6'>johndoe@gmail.com</span>
             </div>
             <div className='flex flex-row p-1.5'>
-              <img src={process.env.PUBLIC_URL + 'images/Phone.svg'} alt='' />{' '}
+              <PhoneIcon className='w-6 h-6' />{' '}
               <span className='pl-6'>09123456789</span>
             </div>
           </div>
@@ -46,17 +54,14 @@ const UserInformation: FC = () => {
               type='submit'
               className='border-gray-700 border-2 hover:border-gray-500 rounded-md flex w-full items-center justify-center p-2 mt-6 text-sm space-x-2 '
             >
-              <img
-                src={process.env.PUBLIC_URL + 'images/Download.svg'}
-                alt=''
-              />
+              <ArrowDownTrayIcon className='w-6 h-6' />
               <span className='text-gray-700'>Download QR Code</span>
             </button>
             <button
               type='submit'
               className='bg-blue-gray-700 border-2 hover:border-gray-500 rounded-md flex w-full items-center justify-center p-2 mt-6 text-sm space-x-2 !'
             >
-              <img src={process.env.PUBLIC_URL + 'images/Archive.svg'} alt='' />
+              <ArchiveBoxArrowDownIcon className='w-6 h-6' />
               <span className='text-current'>Download QR Code</span>
             </button>
           </div>
