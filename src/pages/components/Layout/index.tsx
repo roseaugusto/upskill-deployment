28e0924e7d350973203695f567eea91704b3e6a3
Rogelio John Oliverio <rogeliojohn.oliverio@sun-asterisk.com>
2023-05-11 09:48:42 +0800

    Reposition toast to top-center

--- src/pages/components/Layout/index.tsx
+++ src/pages/components/Layout/index.tsx
@@ -9,7 +9,7 @@ const Layout: FC<Props> = ({ children }) => {
   return (
     <div className='flex items-center justify-center h-screen min-h-screen overflow-auto bg-slate-50 text-slate-700'>
       {children}
-      <ToastContainer position='bottom-left' />
+      <ToastContainer position='top-center' />
     </div>
   );
 };
